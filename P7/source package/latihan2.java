d0b7f33017bdf1b407fc20ceeab39b92335c3f4e
Syifa Aulia <emailkamu@gmail.com>
2026-04-20 12:26:04 +0700

    update tugas1

--- P7/source package/latihan2.java
+++ P7/source package/latihan2.java
@@ -5,21 +5,21 @@ public class latihan2
     {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Pilihan menu:");
-        System.out.println("a. Mie ayam");
-        System.out.println("b. bakso");
-        System.out.print("Pilih menu:");
+        System.out.println("Pilihan :");
+        System.out.println("a. Ayam ");
+        System.out.println("b. Beuang");
+        System.out.print("Pilih hewan:");
         String pilihan = input.nextLine();
 
         switch(pilihan){
             case "a":
-                System.out.println("Mie ayam");
+                System.out.println("Ayam");
                 break;
             case "b":
-                System.out.println("Bakso");
+                System.out.println("Beruang");
                 break;
             default:
-                System.out.println("Menu tidak tersedia");
+                System.out.println("Hewan tidak tersedia");
         }
     System.out.println("Pilihan kamu: " + pilihan);
     input.close();
